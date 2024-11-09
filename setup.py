2f7d195a88d22c3e8beb39e0b149ba548e05168e
saikiran2508 <gsaikiran502@gmail.com>
2024-11-08 20:41:28 -0500

    requirements & installation done

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md","r",encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Text_Summarizer_End_to_End"
+AUTHOR_USER_NAME = "saikiran2508"
+SRC_REPO = "Test_Summarizer"
+AUTHOR_EMAIL = "gsaikiran502@gmail.com"
+
+setuptools.setup(
+    name = SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="Text summarization",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker" : f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+
+)
